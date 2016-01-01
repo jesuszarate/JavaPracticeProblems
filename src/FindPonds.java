c11314cd5457db33fb691dc85ccf5a24fa689501
Jesus <chuy8jay@gmail.com>
2015-12-31 18:26:23 -0700

    Added description to the pond problem, with exmple input and output.

--- src/FindPonds.java
+++ src/FindPonds.java
@@ -1,5 +1,21 @@
 /**
- * Created by jesuszarate on 12/30/15.
+ * Created by Jesus Zarate on 12/30/15.
+ *
+ * POND SIZES: You have an integer matrix representing a plot of land, where the value at that
+ * location represents the height above sea level. A value of zero indicates water. A pond is
+ * a region of water connected vertically, horizontally, or diagonally. The size of the pond
+ * is the total number of connected water cells. Write a method to compute the sizes of all
+ * ponds in the matrix.
+ *
+ * Example
+ * input:
+ *  0 2 1 0
+ *  0 1 0 1
+ *  1 1 0 1
+ *  0 1 0 1
+ *
+ *  Output: 2, 4, 1 (in any order)
+ *
  */
 public class FindPonds
 {
