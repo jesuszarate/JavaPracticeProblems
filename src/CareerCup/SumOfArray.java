b5f7d85f58f19523b319f9322d1031b6e6963d79
Jesus <chuy8jay@gmail.com>
2016-01-07 21:26:34 -0700

    Fixed carry bug and added tests to sum of array.

--- src/CareerCup/SumOfArray.java
+++ src/CareerCup/SumOfArray.java
@@ -23,23 +23,43 @@ public class SumOfArray
         int[] arr1 = new int[]{1,2,3};
         int[] arr2 = new int[]{2,3,4};
 
-        for(int i : arraySum(arr1, arr2))
+        printArr(arraySum(arr1, arr2), "[3 5 7 ]");
+
+        arr1 = new int[]  {1,2,3};
+        arr2 = new int[]{2,3,5,5};
+
+        System.out.println();
+        printArr(arraySum(arr1, arr2), "[2 4 7 8 ]");
+
+        arr1 = new int[]{9,9,2};
+        arr2 = new int[]{0,1,3};
+
+        System.out.println();
+        printArr(arraySum(arr1, arr2), "[1 0 0 5 ]");
+
+    }
+
+    public static void printArr(int[] arr, String result)
+    {
+        System.out.print("[");
+        for(int i : arr)
         {
             System.out.print(i + " ");
         }
-        System.out.println();
+
+        System.out.print("]");
+        System.out.println(" => " + result);
     }
 
     public static int[] arraySum(int[] arr1, int[] arr2)
     {
         Stack<Integer> result = new Stack<Integer>();
 
-        int size =  Math.max(arr1.length, arr2.length);
         int arr1End = arr1.length - 1;
         int arr2End = arr2.length - 1;
 
         int carry = 0;
-        int place = 0;
+        int place;
         while (arr1End >= 0 && arr2End >= 0)
         {
             int sum = arr1[arr1End] + arr2[arr2End] + carry;
@@ -52,16 +72,26 @@ public class SumOfArray
             arr2End--;
         }
 
-        if(arr1End > 0)
+        if(arr1End >= 0)
         {
             for (int i = arr1End; i >= 0; i--)
-                result.push(i);
+            {
+                result.push(arr1[i] + carry);
+                carry = 0;
+            }
         }
-        else if(arr2End > 0)
+        else if(arr2End >= 0)
         {
             for (int i = arr2End; i >= 0; i--)
-                result.push(i);
+            {
+                result.push(arr2[i] + carry);
+                carry = 0;
+            }
+
         }
+
+        if(carry > 0)
+            result.push(carry);
 
         int[] arrRes = new int[result.size()];
         for(int i = 0; i < arrRes.length; i++)
