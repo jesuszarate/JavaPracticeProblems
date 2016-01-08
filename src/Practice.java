e7ac3f8c6e6ab7f4838209a47d8e287122b3f63e
Jesus <chuy8jay@gmail.com>
2016-01-08 14:07:26 -0700

    Added code from Google interview.

---
+++ src/Practice.java
@@ -0,0 +1,95 @@
+/**
+ * Created by Jesus Zarate on 1/8/16.
+ */
+public class Practice
+{
+}
+/*
+
+
+//Please use this Google doc to code during your interview. To free your hands for coding, we recommend that you use a headset or a phone with speaker option.
+
+    number of integers to return, max value
+
+        collection of unique random integers
+
+public HashSet<Integers> randomCollection(int numOfInts, int maxVal){
+
+        Random r = new Random();
+
+        HashSet<Integers> results = new HashSet<Integers>();
+        for(int i = 0; i < numOfInts; i++)
+        {
+        int temp = r.nextInt(maxVal);
+        while (result.contains(temp)){
+        temp = r.nextInt(maxVal);
+        }
+        results.add(temp);
+        }
+
+        if(results.length < numOfInts)
+        {
+        throw new Exception(“Cannot return the specified number of ints”);
+        }
+        return results;
+        }
+
+        10,  => 20
+        10, 5 =>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        hasNext =>
+        next =>
+
+        Iterator constructor, iterator
+
+class myIterator<T> implements Iteratot<T>{
+
+    Iterator<T> itr;
+    public myIterator(Interator<T> _itr){
+        this.itr = _itr;
+    }
+
+    @Override
+    public boolean hasNext(){
+
+        if(itr.hasNext()) return true;
+
+        if(!itr.hasNext())return false;
+
+    }
+
+    @Override
+    public T next(){
+
+        T current;
+        if(itr.hasNext())
+            current = itr.next();
+
+        while(current == null)
+        {
+            if(itr.hasNext()){
+                T temp = itr.next();
+                if(temp == null){
+                    current = itr.next();
+                }
+            }
+        }
+        return next();
+    }
+ */
