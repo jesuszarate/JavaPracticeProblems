316a8d3fdb89baf03facd8a6df19af6d85530e72
Jesus <chuy8jay@gmail.com>
2016-12-14 13:26:01 -0700

    Better implementation

--- src/HackerRank/LarrysArray/sumEqualsXor/sumEqualsXor.java
+++ src/HackerRank/LarrysArray/sumEqualsXor/sumEqualsXor.java
@@ -11,20 +11,14 @@ public class sumEqualsXor {
         Scanner in = new Scanner(System.in);
         long n = in.nextLong();
 
-        int count = 0;
+        long count = 0;
 
-        if (n >= 0 && n <= Math.pow(10, 15)) {
-            for (int i = 0; i <= n; i++) {
-                long res = n + i;
-                if ((n ^ i) == res) {
-                    System.out.println(i);
-                    count++;
-                }
-            }
-            System.out.println("Count: " + count);
-        } else {
-            System.out.println(n);
+        while(n > 0){
+            count += (n % 2 == 1)? 0 : 1;
+            n /= 2;
         }
+
+        System.out.printf((long)Math.pow(2, count) + "");
     }
 
 }
