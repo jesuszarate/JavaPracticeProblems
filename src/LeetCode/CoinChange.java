4ab898f000cd49b6858d9a2756b43d04ce99fa4d
Jesus <chuy8jay@gmail.com>
2017-05-10 01:36:26 -0600

    Set up new folder for moderate difficulty question in Cracking the Coding interview book.

--- src/LeetCode/CoinChange.java
+++ src/LeetCode/CoinChange.java
@@ -1,6 +1,8 @@
 package LeetCode;
 
+import java.util.BitSet;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * Created by jesuszarate on 4/13/17.
@@ -9,6 +11,45 @@ public class CoinChange {
     public static HashMap<Integer, Integer> coinMap = new HashMap<Integer, Integer>();
     public static void main(String[] a) {
         System.out.println(coinChange(new int[]{1,2,5}, 11));
+
+
+        LinkedList<Integer> lk = new LinkedList<Integer>();
+        // 1 0 1 1
+
+        // 0 1 2 3
+        // 0 0 0 0
+        // set(1)
+        // 0 0 0 1
+        // 0 0 0 0 ||
+        // 0 0 0 1
+
+        // set(3)
+        // 0 1 0 1
+
+        lk.add(1);
+        lk.add(0);
+        lk.add(1);
+        lk.add(1);
+
+        BitSet bitSet = new BitSet();
+
+
+        for (int i = 0; i < lk.size(); i++){
+            if(lk.get(i) == 1)
+                bitSet.set(i);
+        }
+
+        for(int i = 0; i < bitSet.length(); i++){
+            System.out.println(bitSet.get(i));
+        }
+
+
+        System.out.println();
+        bitSet.set(3);
+        for(int i = 0; i < bitSet.length(); i++){
+            System.out.println(bitSet.get(i));
+        }
+
     }
 
     public static int coinChange(int[] coins, int amount) {
---
+++ src/ModerateCCI/SmallestDifference.java
@@ -0,0 +1,15 @@
+package ModerateCCI;
+
+/**
+ * Created by jesuszarate on 5/10/17.
+ */
+public class SmallestDifference {
+
+    public static void main(String[] a){
+
+    }
+
+    public static int smallestDiff(int[] a1, int[] a2){
+        return -1;
+    }
+}
